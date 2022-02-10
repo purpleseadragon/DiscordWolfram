447c676a834e7d502394901b2d8b22f339a06525
Oscar Davine <oscardavine@gmail.com>
2022-02-10 22:57:09 +1000

    testing bot

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+with open('token.txt') as token:
+    TOKEN = token.readlines()[0]
+client.run(TOKEN)
+
 print('hellow word')
