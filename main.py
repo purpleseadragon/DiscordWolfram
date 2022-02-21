7e8799fbb7863cc62541bf6839bfb7404cfe10ae
Oscar Davine <oscardavine@gmail.com>
2022-02-21 16:30:12 +1000

    added lead pipe feature

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ async def on_message(message):
 
     elif message.content.startswith('$meme'):
         # uploads random meme from folder
-        random_meme=random.choice(os.listdir("D:\meme_folder"))
-        await message.channel.send(file=discord.File(f'D:\meme_folder\{random_meme}'))
+        random_meme=random.choice(os.listdir("D:\god_folder\meme_folder"))
+        await message.channel.send(file=discord.File(f'D:\god_folder\meme_folder\{random_meme}'))
+    
+    elif message.content.startswith('$leadpipe'):
+        random_meme=random.choice(os.listdir("D:\god_folder\lead_pipe"))
+        await message.channel.send(file=discord.File(f'D:\god_folder\lead_pipe\{random_meme}'))
 
     elif message.content.startswith('$wolf '):
         rest_of_message = message.content[6:]
@@ -28,11 +32,13 @@ async def on_message(message):
         except StopIteration:
             await message.channel.send('That was not a valid query')
 
+
     elif message.content.startswith('$help'):
         await message.channel.send('The bot currently recognises the following commands: \n \
     **$meme** will generate a random meme \n \
     **$wolf** followed by a query will return the answer given by wolfram alpha \n \
     **$hello** will cause the bot to reply with a greeting \n \
+    **$leadpipe** will post a god meme \n \
     **$help** will print this message.')
 
 
